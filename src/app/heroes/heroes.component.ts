1ce3fba2735080d4e6fe229e588162897a035337
LukasErdmanski <lukas.erdmanski@yahoo.com>
2023-11-02 12:28:01 +0100

    1. The hero editor: Show the HeroesComponent view COMPLETED

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+/**
+ * You always import the Component symbol from the Angular core library and annotate the component class with @Component.
+ * @Component is a decorator function that specifies the Angular metadata for the component.
+ */
+@Component({
+  /**
+   * The CSS element selector, 'app-heroes', matches the name of the HTML element that identifies this component within
+   * a parent component's template.
+   */
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+// Always export the component class so you can import it elsewhere … like in the AppModule.
+export class HeroesComponent {
+  hero = 'Windsorm';
+}
