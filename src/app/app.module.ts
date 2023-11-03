61da962921772908f08ea2faf787cc02a3520a8d
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-11-03 05:51:51 +0100

    1. The hero editor COMPLETED: Declare the HeroesComponent COMPLETED"
    Summary:
    - You used ng generate to create a second HeroesComponent.
    - You displayed the HeroesComponent by adding it to the AppComponent shell.
    - You applied the UppercasePipe to format the name.
    - You used two-way data binding with the ngModel directive.
    - You learned about the AppModule.
    - You imported the FormsModule in the AppModule so that Angular would recognize and apply the ngModel directive.
    - You learned the importance of declaring components in the AppModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+// Required for ngModel, two-way data binding between the <input> form element in the template and the property in the ts-class.
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+/**
+ * Angular needs to know how the pieces of your application fit together and what other files and libraries
+ * the application requires. This information is called metadata.
+ * Some of the metadata is in the @Component decorators that you added to your component classes.
+ * Other critical metadata is in @NgModule decorators.
+ * The most important @NgModule decorator annotates the top-level AppModule class.
+ */
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    /**
+     * Every component must be declared in exactly one NgModule.
+     *
+     * You didn't declare the HeroesComponent. Why did the application work?
+     * It worked because the ng generate declared HeroesComponent in AppModule when it created that component.
+     */
+    declarations: [AppComponent, HeroesComponent],
+    // The imports array contains the list of external modules that the application needs.
+    imports: [BrowserModule, FormsModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
