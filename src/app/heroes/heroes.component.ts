045eae770a1b48e3831c579b80de4d931d31f8ee
Lukas Erdmanski <lukas.erdmanski@yahoo.com>
2023-11-03 06:12:21 +0100

    2. Display a list: Style the heroes COMPLETED

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 /**
  * You always import the Component symbol from the Angular core library and annotate the component class with @Component.
@@ -12,12 +13,18 @@ import { Hero } from '../hero';
      */
     selector: 'app-heroes',
     templateUrl: './heroes.component.html',
+    /**
+     * You define private styles either inline in the @Component.styles array or as style sheet files identified in
+     * the @Component.styleUrls array.
+     * When the ng generate created the HeroesComponent, it created an empty heroes.component.css style sheet for
+     * the HeroesComponent and pointed to it in @Component.styleUrls like this.
+     * Styles and style sheets identified in @Component metadata are scoped to that specific component.
+     * The heroes.component.css styles apply only to the HeroesComponent and don't affect the outer HTML or
+     * the HTML in any other component.
+     */
     styleUrls: ['./heroes.component.css'],
 })
 // Always export the component class so you can import it elsewhere … like in the AppModule.
 export class HeroesComponent {
-    hero: Hero = {
-        id: 1,
-        name: 'Windsorm',
-    };
+    heroes = HEROES;
 }
